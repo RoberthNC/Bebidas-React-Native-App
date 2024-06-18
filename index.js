b37a1b6843e95d0e32197fad3cef953e01fca87f
Roberto Núñez <robertonc1599@gmail.com>
2024-06-17 19:17:03 -0500

    Project Base: Folder structure

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import {BebidasApp} from './src/BebidasApp';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => BebidasApp);
---
+++ src/BebidasApp.tsx
@@ -0,0 +1,9 @@
+import {Text, View} from 'react-native';
+
+export const BebidasApp = () => {
+  return (
+    <View>
+      <Text>BebidasApp</Text>
+    </View>
+  );
+};
